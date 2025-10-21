d9999e8edca266c991b71df5391cdae74b5b151c
ShujaAliSattar-Dev <shuja.ali@hatzs.com>
2025-10-21 19:49:44 +0500

    fix: correct TitleCards CSS import casing

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect, useRef, useState  } from 'react'
-import './Titlecards.css'
+import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 import { Link } from 'react-router-dom'
 
