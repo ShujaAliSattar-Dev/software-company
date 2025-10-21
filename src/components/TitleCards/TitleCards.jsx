c7b37e09d7359f1c27519e43e2e05a8d87242291
ShujaAliSattar-Dev <shuja.ali@hatzs.com>
2025-10-21 18:38:31 +0500

    Netflix

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { useEffect, useRef, useState  } from 'react'
+import './Titlecards.css'
+import cards_data from '../../assets/cards/Cards_data'
+import { Link } from 'react-router-dom'
+
+const TitleCards = ({title, category}) => {
+
+
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef();
+
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNWU3YTY3NWI2ZTQ5ZTI0N2I5OGI3MjUwZDYyNzIyOCIsIm5iZiI6MTc1NjI3ODcyNC4xNjk5OTk4LCJzdWIiOiI2OGFlYWZjNDJiYmNjYmQ0YTdiMzZmYjYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.HInBuFKUrwA6GIHwjhduIJWsIISGPKHy20C4jVTZsD8'
+  }
+};
+
+
+
+  
+  
+
+  const handleWheel = (event) => {  
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;  
+  }
+
+  useEffect(()=>{
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+    cardsRef.current.addEventListener('wheel', handleWheel);
+  },[]);
+
+
+  return (
+    <div className="title-cards">
+      <h2>{title?title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index) => {
+          return (
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+              <p>{card.original_title}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default TitleCards;
